b0205a24b5a1b8ae208d372ad9e53d4ec225be33
NavaCorp26 <luisnavarrete1a89@gmail.com>
2025-10-26 23:24:31 -0500

    Refactor: Refactorizacion de codigo vario

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from api.patients.models import Paciente
 
-# Register your models here.
+@admin.register(Paciente)
+class PacienteAdmin(admin.ModelAdmin):
+    list_display = (
+        'id_paciente', 'nombres', 'apellidos', 'cedula_pasaporte',
+        'fecha_nacimiento', 'sexo', 'telefono', 'correo',
+        'contacto_emergencia_nombre', 'contacto_emergencia_telefono',
+        'status', 'created_at'
+    )
+    list_filter = ('sexo', 'status', 'created_at')
+    search_fields = ('nombres', 'apellidos', 'cedula_pasaporte', 'telefono', 'correo')
+    readonly_fields = ('created_by', 'updated_by', 'created_at', 'updated_at')
+
+    fieldsets = (
+        ('Datos Personales', {
+            'fields': (
+                'nombres', 'apellidos', 'cedula_pasaporte', 'fecha_nacimiento', 'sexo', 'direccion',
+                'telefono', 'correo'
+            )
+        }),
+        ('Emergencia', {
+            'fields': (
+                'contacto_emergencia_nombre', 'contacto_emergencia_telefono'
+            )
+        }),
+        ('Seguridad', {
+            'fields': (
+                'alergias', 'enfermedades_sistemicas', 'habitos'
+            )
+        }),
+        ('Auditoría', {
+            'fields': (
+                'status', 'created_by', 'updated_by', 'created_at', 'updated_at'
+            )
+        }),
+    )
---
+++ api/odontogram/admin.py
@@ -0,0 +1,31 @@
+
+# api/odontogram/admin.py
+from django.contrib import admin
+from .models import (
+    CategoriaDiagnostico,
+    Diagnostico,
+    AreaAfectada,
+    DiagnosticoAreaAfectada,
+    TipoAtributoClinico,
+    OpcionAtributoClinico,
+    DiagnosticoAtributoClinico,
+    Paciente,
+    Diente,
+    SuperficieDental,
+    DiagnosticoDental,
+    HistorialOdontograma
+)
+
+# Registro básico (mostrar en admin)
+admin.site.register(CategoriaDiagnostico)
+admin.site.register(Diagnostico)
+admin.site.register(AreaAfectada)
+admin.site.register(DiagnosticoAreaAfectada)
+admin.site.register(TipoAtributoClinico)
+admin.site.register(OpcionAtributoClinico)
+admin.site.register(DiagnosticoAtributoClinico)
+admin.site.register(Paciente)
+admin.site.register(Diente)
+admin.site.register(SuperficieDental)
+admin.site.register(DiagnosticoDental)
+admin.site.register(HistorialOdontograma)
