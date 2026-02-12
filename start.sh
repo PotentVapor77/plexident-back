84668d2bc01db77b40b3d3286d49eeb0fb515a2a
NavaCorp26 <luisnavarrete1a89@gmail.com>
2026-02-11 21:49:59 -0500

    modified:   start.sh

--- start.sh
+++ start.sh
@@ -2,11 +2,10 @@
 set -e
 
 echo "==> Aplicando migraciones..."
-python manage.py migrate --noinput
+python3 manage.py migrate --noinput
 
 echo "==> Recolectando estáticos..."
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 echo "==> Iniciando Gunicorn..."
-# IMPORTANTE: Usar 'python -m gunicorn'
-exec python -m gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 3
+exec python3 -m gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 3
